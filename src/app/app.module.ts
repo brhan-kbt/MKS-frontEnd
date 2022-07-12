f6387f8f19a16df0a2d6ae4eaf8db825f72d7e6e
Henok Zegeye <henokzegeye1990@gmail.com>
2022-07-12 12:33:27 +0300

    Configuration changes -see TMC-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SetupModule } from './setup/setup.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SetupModule } from './setup/setup.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SetupModule
+    SetupModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
