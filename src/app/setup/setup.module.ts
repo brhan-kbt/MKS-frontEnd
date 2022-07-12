f57624da03b07fe88bfcdf2320c4b7d0513bf714
Henok Zegeye <henokzegeye1990@gmail.com>
2022-07-12 12:05:47 +0300

    Setup module generated -see TMC-1

---
+++ src/app/setup/setup.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SetupRoutingModule } from './setup-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SetupRoutingModule
+  ]
+})
+export class SetupModule { }
