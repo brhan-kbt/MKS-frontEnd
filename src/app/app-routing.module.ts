f42a2ae00ab9b24efc2e7123695abdef27a6286a
aaabrsh <aaabrham421@gmail.com>
2022-07-21 09:19:16 +0300

    Transfer-offer-item route added -see FET-9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TransportOfferItemComponent } from './setup/components/container/transport-offer-item/transport-offer-item.component';
 import { TransportOfferComponent } from './setup/components/container/transport-offer/transport-offer.component';
 import { TransporterComponent } from './setup/components/container/transporter/transporter.component';
 
 const routes: Routes = [
   {path: 'transporters', component: TransporterComponent},
-  {path: 'transport-offer', component: TransportOfferComponent}
+  {path: 'transport-offer', component: TransportOfferComponent},
+  {path: 'transport-offer/:id', component: TransportOfferItemComponent}
 ];
 
 @NgModule({
